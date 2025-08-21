1aa957c59c99f180abbb142ff336bc4f3a148f82
EmanuelRigo <112441019+EmanuelRigo@users.noreply.github.com>
2025-08-21 17:27:00 -0300

    adding roles

--- init/06_roles.sql
+++ init/06_roles.sql
@@ -3,25 +3,36 @@ USE lab_db_sql;
 -- Roles (MySQL 8+)
 CREATE ROLE IF NOT EXISTS role_admin;
 CREATE ROLE IF NOT EXISTS role_receptionist;
+CREATE ROLE IF NOT EXISTS role_lab_technician;
 CREATE ROLE IF NOT EXISTS role_biochemist;
 
 -- Permisos por rol
+
 -- Admin: todo
-GRANT ALL PRIVILEGES ON lab_db_sql.* TO role_admin;
+GRANT SELECT, INSERT, UPDATE         ON lab_db_sql.MedicalStudy        TO role_admin;
+GRANT SELECT                         ON lab_db_sql.Result              TO role_admin;
+GRANT SELECT, INSERT, UPDATE, DELETE ON lab_db_sql.LabStaff            TO role_admin;
+GRANT SELECT, INSERT, UPDATE, DELETE ON lab_db_sql.Patient             TO role_admin;
+GRANT SELECT,         UPDATE         ON lab_db_sql.DoctorAppointment   TO role_admin;
+GRANT SELECT,                        ON lab_db_sql.Payment             TO role_admin;
+GRANT SELECT,                        ON lab_db_sql.Talon               TO role_admin;
 
 -- Receptionist: pacientes, citas, talones, pagos (CRUD básico)
-GRANT SELECT, INSERT, UPDATE ON lab_db_sql.Patient            TO role_receptionist;
-GRANT SELECT, INSERT, UPDATE ON lab_db_sql.DoctorAppointment  TO role_receptionist;
-GRANT SELECT, INSERT, UPDATE ON lab_db_sql.Talon              TO role_receptionist;
-GRANT SELECT, INSERT        ON lab_db_sql.Payment            TO role_receptionist;
-GRANT SELECT                ON lab_db_sql.MedicalStudy       TO role_receptionist;
-GRANT SELECT                ON lab_db_sql.v_appointments_full TO role_receptionist;
+GRANT SELECT                         ON lab_db_sql.MedicalStudy        TO role_receptionist;
+GRANT SELECT                         ON lab_db_sql.Result              TO role_receptionist;
+GRANT SELECT, INSERT, UPDATE         ON lab_db_sql.Patient             TO role_receptionist;
+GRANT SELECT, INSERT, UPDATE         ON lab_db_sql.DoctorAppointment   TO role_receptionist;
+GRANT SELECT, INSERT                 ON lab_db_sql.Payment             TO role_receptionist;
+GRANT SELECT, INSERT                 ON lab_db_sql.Talon               TO role_receptionist;
+GRANT SELECT                         ON lab_db_sql.v_appointments_full TO role_receptionist;
+
+-- LabTechnician: pacientes, citas, talones, pagos (CRUD básico)
+GRANT SELECT                         ON lab_db_sql.MedicalStudy        TO role_receptionist;
+GRANT SELECT, INSERT, UPDATE         ON lab_db_sql.Result              TO role_receptionist;
+GRANT SELECT,         UPDATE         ON lab_db_sql.DoctorAppointment   TO role_receptionist;
 
 -- Biochemist: resultados (crear/editar) y lectura de lo demás
-GRANT SELECT                          ON lab_db_sql.Patient           TO role_biochemist;
-GRANT SELECT                          ON lab_db_sql.DoctorAppointment TO role_biochemist;
-GRANT SELECT, INSERT, UPDATE          ON lab_db_sql.Result            TO role_biochemist;
-GRANT SELECT                          ON lab_db_sql.v_results_full    TO role_biochemist;
+GRANT SELECT, INSERT, UPDATE         ON lab_db_sql.Result              TO role_biochemist;
 
 -- Usuarios de ejemplo (cámbiales la contraseña)
 CREATE USER IF NOT EXISTS 'app_admin'@'%' IDENTIFIED BY 'ChangeMe_admin!';
